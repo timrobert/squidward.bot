103f40a752dd3f14fd1c12d4b3f6d1b4b231c79c
Tim <tim@metalcowrobotics.com>
2023-07-29 23:32:12 -0500

    echos to printf

--- sendWeeklyClsaEmail.sh
+++ sendWeeklyClsaEmail.sh
@@ -3,7 +3,7 @@
 retry_limit=5
 retry_count=0
 
-echo "Sending CLSA Weekly Email..."
+printf "Sending CLSA Weekly Email..."
 
 while [ $retry_count -lt $retry_limit ]
 do
@@ -13,19 +13,19 @@ do
   
   if [ $exit_code -eq 0 ]
   then
-    echo "SUCCESS: Email script executed successfully."
+    printf "SUCCESS: Email script executed successfully."
     break
   else
-    echo "WARN: Email script exited with non-zero exit code. Retrying..."
+    printf "WARN: Email script exited with non-zero exit code. Retrying..."
     retry_count=$((retry_count-1))
     sleep_count=$((retry_count * 60 * 60))  # Calculate the sleep time, delay in 1hr increments
-    echo "\tWaiting for $sleep_count seconds..."
+    printf "\tWaiting for %s seconds..." "$sleep_count"
     sleep "$sleep_count"
   fi
 done
 
 if [ $retry_count -ge $retry_limit ]
 then
-  echo "ERROR: Exceeded retry limit. Node script execution failed."
+  printf "ERROR: Exceeded retry limit. Node script execution failed."
   exit 1
 fi

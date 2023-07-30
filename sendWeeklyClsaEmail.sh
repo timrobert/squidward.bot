cd3e1a302d55cf3f2ee5f52f17d7c881d8fc741e
Tim <tim@metalcowrobotics.com>
2023-07-29 23:36:56 -0500

    fixing stuff and retry count

--- sendWeeklyClsaEmail.sh
+++ sendWeeklyClsaEmail.sh
@@ -17,7 +17,7 @@ do
     break
   else
     printf "WARN: Email script exited with non-zero exit code. Retrying..."
-    retry_count=$((retry_count-1))
+    retry_count=$((retry_count + 1))
     sleep_count=$((retry_count * 60 * 60))  # Calculate the sleep time, delay in 1hr increments
     printf "\tWaiting for %s seconds..." "$sleep_count"
     sleep "$sleep_count"
